6fcc7e3db10fc26211c514cbedb39cc5f4007ae1
Amanda Westin <69770137+a-westin@users.noreply.github.com>
2020-10-28 20:24:11 -0400

    added if/else statement

--- app.js
+++ app.js
@@ -1,22 +1,45 @@
 const inquirer = require("inquirer");
 
 function initApp() {
-  inquirer.prompt({
-    type: "list",
-    message: "Please make a selection.",
-    name: "action",
-    choices: [
-      "View all employees",
-      "View all employees by Department",
-      "View all employees by Manager",
-      "View all Roles",
-      "Add an employee",
-      "Remove an employee",
-      "Update an employee Role",
-      "Update an employee Manager",
-      "Quit",
-    ],
-  });
+  inquirer
+    .prompt({
+      type: "list",
+      message: "Please make a selection.",
+      name: "action",
+      choices: [
+        "View all employees",
+        "View all employees by department",
+        "View all employees by manager",
+        "View all roles",
+        "Add an employee",
+        "Remove an employee",
+        "Update an employee role",
+        "Update an employee manager",
+        "Quit",
+      ],
+    })
+    .then((res) => {
+      if (res.action === "View all employees") {
+        viewAllEmployees();
+      } else if (res.action === "View all employees by department") {
+        employeesByDepartment();
+      } else if (res.action === "View all employees by manager") {
+        employeesByManager();
+      } else if (res.action === "View all roles") {
+        viewAllRoles();
+      } else if (res.action === "Add an employee") {
+        addEmployee();
+      } else if (res.action === "Remove an employee") {
+        removeEmployee();
+      } else if (res.action === "Update an employee role") {
+        updateEmployeeRole();
+      } else if (res.action === "Update an employee manager") {
+        updateManager();
+      } else if (res.action === "Quit") {
+        return;
+      }
+    })
+    .catch((err) => console.log(err));
 }
 
 initApp();
