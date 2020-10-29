994c49cfaa29a1f41ca4cd65ee350ae0b1db68b9
Amanda Westin <69770137+a-westin@users.noreply.github.com>
2020-10-28 20:34:37 -0400

    added functions for each menu selection

--- app.js
+++ app.js
@@ -20,26 +20,59 @@ function initApp() {
     })
     .then((res) => {
       if (res.action === "View all employees") {
-        viewAllEmployees();
+        viewAllEmp();
       } else if (res.action === "View all employees by department") {
-        employeesByDepartment();
+        empByDept();
       } else if (res.action === "View all employees by manager") {
-        employeesByManager();
+        empByMgr();
       } else if (res.action === "View all roles") {
         viewAllRoles();
       } else if (res.action === "Add an employee") {
-        addEmployee();
+        addEmp();
       } else if (res.action === "Remove an employee") {
-        removeEmployee();
+        removeEmp();
       } else if (res.action === "Update an employee role") {
-        updateEmployeeRole();
+        updateEmpRole();
       } else if (res.action === "Update an employee manager") {
-        updateManager();
+        updateEmpMgr();
       } else if (res.action === "Quit") {
         return;
       }
     })
     .catch((err) => console.log(err));
+}
+
+// Console logging whichever selection the user makes
+function viewAllEmp() {
+  console.log("View all employees");
+}
+
+function empByDept() {
+  console.log("View all employees by department");
+}
+
+function empByMgr() {
+  console.log("View all employees by manager");
+}
+
+function viewAllRoles() {
+  console.log("View all roles");
+}
+
+function addEmp() {
+  console.log("Add an employee");
+}
+
+function removeEmp() {
+  console.log("Remove an employee");
+}
+
+function updateEmpRole() {
+  console.log("Update an employee role");
+}
+
+function updateEmpMgr() {
+  console.log("Update employee manager");
 }
 
 initApp();
